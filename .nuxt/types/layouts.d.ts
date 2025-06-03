cb0b6332737c1470b79cf104f8e21dfb4e576a3e
zayn <zy.isakovich18@gmail.com>
2025-06-03 18:28:50 +0500

    Versioning untracked files

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "custom-layout" | "default" | "main-layout"
-declare module 'nuxt/app' {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
